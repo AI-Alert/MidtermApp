4a10a9915f6b0e7ede5ce98ef7ff257336d55d21
AI-Alert <50011985+AI-Alert@users.noreply.github.com>
2022-04-26 19:25:20 +0200

    Third Commit

--- app/src/main/java/com/example/midtermapp/Group_fragment.java
+++ app/src/main/java/com/example/midtermapp/Group_fragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -20,8 +21,10 @@ public class Group_fragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_group, container, false);
-
     }
+
+
+
 
 
 }
